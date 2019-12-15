dc64dbc13ea53627714597b9e1b22c9c4f80c894
tony <liboliqi@gmail.com>
2019-12-15 22:19:55 +0800

    buyback theft: link to account

--- entity/buyback-theft.ts
+++ entity/buyback-theft.ts
@@ -1,8 +1,13 @@
-import { Entity, PrimaryColumn } from 'typeorm'
+import { Entity, PrimaryColumn, OneToOne, JoinColumn } from 'typeorm'
 import { fixedBytes } from '../transformers'
+import { Account } from './account'
 
 @Entity()
 export class BuybackTheft {
     @PrimaryColumn({ type: 'binary', length: 20, transformer: fixedBytes(20, 'buyback.address') })
     public address!: string
+
+    @OneToOne(type => Account)
+    @JoinColumn({name: 'address'})
+    public transaction!: Account
 }
