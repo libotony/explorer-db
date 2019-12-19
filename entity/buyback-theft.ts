467aa5dbf602c8c61874b7f244bca8c3a0072131
tony <liboliqi@gmail.com>
2019-12-19 10:47:45 +0800

    remove relations

--- entity/buyback-theft.ts
+++ entity/buyback-theft.ts
@@ -1,13 +1,8 @@
-import { Entity, PrimaryColumn, OneToOne, JoinColumn } from 'typeorm'
+import { Entity, PrimaryColumn } from 'typeorm'
 import { fixedBytes } from '../transformers'
-import { Account } from './account'
 
 @Entity()
 export class BuybackTheft {
     @PrimaryColumn({ type: 'binary', length: 20, transformer: fixedBytes(20, 'buyback.address') })
     public address!: string
-
-    @OneToOne(type => Account)
-    @JoinColumn({name: 'address'})
-    public transaction!: Account
 }
