96495031b3df5f261f4cb829426a3268f9822c88
tony <liboliqi@gmail.com>
2019-12-19 10:49:27 +0800

    remove buyback

--- entity/buyback-theft.ts
+++
@@ -1,8 +0,0 @@
-import { Entity, PrimaryColumn } from 'typeorm'
-import { fixedBytes } from '../transformers'
-
-@Entity()
-export class BuybackTheft {
-    @PrimaryColumn({ type: 'binary', length: 20, transformer: fixedBytes(20, 'buyback.address') })
-    public address!: string
-}
